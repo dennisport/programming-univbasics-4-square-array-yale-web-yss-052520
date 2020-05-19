de2b3c0bf36915ef6915650b0f0544b34b083861
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-19 23:21:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,12 +6,3 @@ def square_array(array)
   squared
 end
 
-
-def square_array(array)
-  new_array = []
-  
-  array.length.times do |index|
-  new_array.push(array[index] ** 2)
-  end
-new_array
-end
